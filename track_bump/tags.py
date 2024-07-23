b98f6d448b1fe3ec5dcf6cf67dd23854840834ec
foo <foo@bar.com>
2024-07-23 11:44:27 +0200

    fix: adding debug information

--- track_bump/tags.py
+++ track_bump/tags.py
@@ -58,7 +58,7 @@ def get_new_tag(latest_tag: str | None, release_tag: ReleaseTag, last_commit_mes
         _tag = f"{_next_release}-{release_tag}.{_release_number}"
     else:
         _last_commit_message = last_commit_message or get_last_commit_message()
-        logger.info(f"Last commit message: {_last_commit_message}")
+        logger.info(f"Last commit message: {_last_commit_message}" if _last_commit_message else "No commit message")
         if _last_commit_message is None or _BUMP_MINOR_REG.match(_last_commit_message):
             logger.debug("Bumping minor")
             _tag = _next_release
